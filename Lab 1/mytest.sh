962d27ac4bf7c0240a58f00fadbef33af8f183a7
sharanjeetsinghmago <sharanjeet.mago@colorado.edu>
2019-10-22 19:30:40 -0600

    OpenMP example

--- Lab 1/mytest.sh
+++ Lab 1/mytest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+: <<'1'
 echo "Hello World"
 
 ./mysort test_case14.txt -t 1 -o cppsoln.txt --alg==fj
@@ -34,4 +35,33 @@ cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0
 
 sort -n test_case14.txt -o solution.txt
 
+cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0 pts )"
+
+./mysort test_case7.txt -t 50 -o cppsoln.txt --alg==fj
+
+sort -n test_case7.txt -o solution.txt
+
+echo " test case 7 t50 fj"
+cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0 pts )"
+
+./mysort test_case7.txt -t 50 -o cppsoln.txt --alg==bucket
+
+sort -n test_case7.txt -o solution.txt
+
+echo " test case 7 t50 bucket"
+cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0 pts )"
+
+./mysort test_case15.txt -t 50 -o cppsoln.txt --alg==fj
+
+sort -n test_case15.txt -o solution.txt
+
+echo " test case 15 t50 fj"
+cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0 pts )"
+
+1
+./mysort test_case15.txt -t 50 -o cppsoln.txt --alg==bucket
+
+sort -n test_case15.txt > solution.txt
+
+echo " test case 15 t50 bucket"
 cmp --silent cppsoln.txt solution.txt && echo " Pass (5 pts )" || echo " Fail (0 pts )"
